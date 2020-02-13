77acd26fd9a59e310cb6b2108575edcbcdd8159f
Daniel Santos <daniels11.dev@gmail.com>
2020-02-13 15:28:02 -0300

    Adicionando o arquivo de criação de Dungeon

---
+++ criarDungeon.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir ./Inicio
+mkdir ./Inicio/Porta_1
+mkdir ./Inicio/.Porta_2
+touch ./Inicio/Porta_1/.me_mate
+mkdir ./Inicio/.Porta_2/Subporta_1
+mkdir ./Inicio/.Porta_2/Subporta_2
+touch ./Inicio/.Porta_2/Subporta_2/Aranha
+touch ./Inicio/.Porta_2/Subporta_2/Aranha2
+touch ./Inicio/.Porta_2/Subporta_2/Aranha3
+touch ./Inicio/.Porta_2/Subporta_2/Aranha4
+touch ./Inicio/.Porta_2/Subporta_1/Mensagem.txt
+echo "Parabens! Voce terminou a dungeon! Agora crie o seu quarto e preencha ele!" > ./Inicio/.Porta_2/Subporta_1/Mensagem.txt
